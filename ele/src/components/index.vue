0be0ae081e342220aa54940320c7c57a62cb10a9
weibin001 <578870507@qq.com>
2018-11-27 15:44:19 +0800

    ele

---
+++ ele/src/components/index.vue
@@ -0,0 +1,21 @@
+<template>
+	<section>
+		<tarbar></tarbar>
+	</section>
+</template>
+
+<script>
+	import tarbar from './tarbar'
+	export default{
+		name:'index',
+		data(){
+			return{}
+		},
+		components:{
+			tarbar
+		}
+	}
+</script>
+
+<style scoped>
+</style>
---
+++ ele/src/components/tarbar.vue
@@ -0,0 +1,48 @@
+<template>
+	<footer class="ele-footer">
+		<router-link tag='a' to='/' activeClass='active'>
+			首页
+		</router-link>
+		<router-link tag='a' to='/' activeClass='active'>
+			首页
+		</router-link>
+		<router-link tag='a' to='/' activeClass='active'>
+			首页
+		</router-link>
+		<router-link tag='a' to='/' activeClass='active'>
+			首页
+		</router-link>
+	</footer>
+</template>
+
+<script>
+export default{
+	name:'tarbar',
+	data(){
+		return{}
+	}
+}
+</script>
+
+<style scoped>
+	.ele-footer{
+		position: fixed;
+		left: 0;
+		bottom: 0;
+		width: 100%;
+		height: 13.33333vw;
+		padding-top: 1.2vw;
+		background-color: #fff;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		z-index: 999;
+	}
+	
+	.ele-footer a{
+		flex: 1;
+		display: inline-block;
+		text-align: center;
+		line-height: 12.13333vw;
+	}
+</style>
